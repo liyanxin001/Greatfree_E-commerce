94e4aaa95016fa34a0b9fc8dd41979af4424e338
liyanxin001 <liyanxin003@126.com>
2026-02-02 21:09:04 +0800

    child

--- ECommerce/src/com/greatfree/cluster/ecommerce/child/StartChild.java
+++ ECommerce/src/com/greatfree/cluster/ecommerce/child/StartChild.java
@@ -6,54 +6,23 @@ import java.util.logging.Logger;
 import org.greatfree.exceptions.RemoteReadException;
 import org.greatfree.util.TerminateSignal;
 
+import com.greatfree.framework.cluster.group.a.child.ChatChildTask;
+
+import edu.greatfree.cluster.ClusterProfile;
 import edu.greatfree.cluster.ClusterSpec;
 import edu.greatfree.cluster.child.Child;
 import edu.greatfree.cluster.child.ChildTask;
+import edu.greatfree.cluster.child.UnaryChild;
 import edu.greatfree.exceptions.RootOfflineException;
 import edu.greatfree.exceptions.TaskAlreadyExistedException;
 
 
- final class StartChild
- {
-   private static final Logger log = Logger.getLogger("edu.greatfree.framework.cluster.mncs.child");
- 
- 
-   
-   private Child child;
- 
- 
-   
-   private static StartChild instance = new StartChild();
- 
-   
-   public static StartChild CLUSTER() {
-     if (instance == null) {
-       
-       instance = new StartChild();
-       return instance;
-     } 
- 
-     
-     return instance;
-   }
- 
- 
-   
-   public void stop() throws ClassNotFoundException, IOException, InterruptedException, RemoteReadException {
-     TerminateSignal.SIGNAL().notifyAllTermination();
-     this.child.stop();
-   }
- 
-   
-   public void start(ClusterSpec spec, ChildTask... tasks) throws ClassNotFoundException, IOException, RemoteReadException, TaskAlreadyExistedException, InterruptedException {
-     this.child = new Child(spec, tasks);
-     
-     try {
-       this.child.start();
-     }
-     catch (RootOfflineException e) {
-       
-       log.info("The root is offline!");
-     } 
-   }
- }
+/*    */ final class StartChild
+/*    */ {
+/*    */   public static void main(String[] args) throws ClassNotFoundException, IOException, RemoteReadException, TaskAlreadyExistedException, InterruptedException {
+/* 24 */     System.out.println("Child starting up ...");
+/* 25 */     UnaryChild.CLUSTER().start(ClusterProfile.getLightChildSpec("Root", 8001, "192.168.1.25", 8941), new ChildTask[] { new MarketChildTask() });
+/* 26 */     System.out.println("Child started ...");
+/* 27 */     TerminateSignal.SIGNAL().waitTermination();
+/*    */   }
+/*    */ }
